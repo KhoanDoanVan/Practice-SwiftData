3a14737ed5c0cc125ccb5597293729c26c2edb56
khoanisdev <doanvankhoan124@gmail.com>
2024-11-25 08:03:08 +0700

    Initial Commit

---
+++ Practice-SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice-SwiftData
+//
+//  Created by Đoàn Văn Khoan on 25/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Practice-SwiftData/Practice_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_SwiftDataApp.swift
+//  Practice-SwiftData
+//
+//  Created by Đoàn Văn Khoan on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
